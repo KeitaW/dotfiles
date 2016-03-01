51369d8ebb6ce774d97bb4f515912d5a273c3edd
Keita Watanabe <keita.watanabe@riken.jp>
2016-03-01 13:18:09 +0900

    add skelton

--- make_link.sh
+++ make_link.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+ln -sf ~/dotfiles/.emacs ~/.emacs
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf 
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.basrc ~/.bashrc
 
