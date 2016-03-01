202c740efa73891d4ad11a9074dc6b56dddc46f9
Keita Watanabe <keita.watanabe@riken.jp>
2016-03-01 15:24:08 +0900

    very basic setting for emacs

---
+++ .emacs.d/inits/00_basic.el
@@ -0,0 +1,6 @@
+;; Disable tool-bar
+(tool-bar-mode 0)
+;; Enable line number
+(global-linum-mode)
+
+
---
+++ .emacs.d/inits/20-package.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+
+;; Install uninstalled packages
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    ;; list of packages you need
+    evil-mode
+    org-mode
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
