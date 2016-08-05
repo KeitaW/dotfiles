b9a88fb2f6482d2b2a976084032e97aee77948a4
keita.watanabe <keitaw09@gmail.com>
2016-08-05 19:53:51 +0900

    add nvim (and .config dir) to VC

--- make_link.sh
+++ make_link.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.emacs ~/.emacs
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/.basrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
+if [ ! -d ~/.config ]
+then
+    mkdir ~/.config
+fi
+ln -s ~/dotfiles/etc/.config/* ~/.config/
