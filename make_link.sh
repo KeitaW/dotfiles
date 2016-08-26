601e84f6b3cd963e8cd40c44def4d5469f733f14
keita.watanabe <keitaw09@gmail.com>
2016-08-26 12:52:26 +0900

    renew .emacs.d

--- make_link.sh
+++ make_link.sh
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
 
 if [ ! -d ~/.config ]
 then
