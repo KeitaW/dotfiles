27a2973e96512db94ebd958823ff6e1bda2ef79c
keita.watanabe <keitaw09@gmail.com>
2016-11-20 14:11:39 +0900

    add init command

--- etc/init/common/vim/dein.sh
+++ etc/init/common/vim/dein.sh
@@ -1,2 +1,2 @@
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh  ../../.config/nvim/dein 
+sh ./installer.sh  ~/dotfiles/.config/nvim/dein 
