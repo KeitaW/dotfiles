2f0bb76caa23c5c5ce8650008d5abd2982967b2b
Keita F. Watanabe <keitaw09@gmail.com>
2017-03-23 14:18:01 +0900

    add vim surround

--- etc/init/common/vim/dein.sh
+++ etc/init/common/vim/dein.sh
@@ -1,3 +1,12 @@
+echo "Dein will be installed in ~/.config"
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+NVIMDIR=~/.config/nvim
+if [ ! -d $NVIMDIR ]
+then
+	echo "No $NVIMDIR found. Create it."
+	mkdir -p $NVIMDIR
+else
+	echo "$NVIMDIR exists. Skip."
+fi
 sh ./installer.sh  ~/.config/nvim/dein 
 rm installer.sh
